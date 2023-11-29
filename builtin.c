e452bb2f62df5b460471de3d198ced77d611ffa8
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-11-29 10:32:47 -0800

    0x16. C - Simple Shell

---
+++ builtin.c
@@ -0,0 +1,125 @@
+#include "shell.h"
+
+/**
+ * _myexit - Exits the shell.
+ * @info: Structure containing potential arguments.
+ *
+ * Return: Exits with a given exit status.
+ */
+int _myexit(info_t *info)
+{
+	int exit_status = 0;
+
+	if (info->argv[1])
+	{
+		exit_status = _erratoi(info->argv[1]);
+		if (exit_status == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1); /* Consider using a symbolic constant for error codes */
+		}
+		info->err_num = exit_status;
+	}
+	else
+	{
+		info->err_num = -1;
+	}
+
+	return (-2);
+}
+
+/**
+ * _handle_cd_no_argument - Handles the case when no argument is provided to cd.
+ * @info: Structure containing potential arguments.
+ */
+static void _handle_cd_no_argument(info_t *info)
+{
+	char *dir, buffer[1024];
+	int chdir_ret;
+
+	char *current_dir = getcwd(buffer, sizeof(buffer));
+	if (!current_dir)
+	{
+		_puts("TODO: Handle getcwd failure\n");
+	}
+
+	dir = _getenv(info, "HOME=");
+	if (!dir)
+	{
+		dir = _getenv(info, "PWD=");
+	}
+
+	chdir_ret = chdir(dir ? dir : "/");
+	_handle_cd_result(info, chdir_ret);
+}
+
+/**
+ * _handle_cd_dash_argument - Handles the case when '-' is provided to cd.
+ * @info: Structure containing potential arguments.
+ */
+static void _handle_cd_dash_argument(info_t *info)
+{
+	char *dir;
+	char buffer[1024];
+	int chdir_ret;
+
+	char *current_dir = getcwd(buffer, sizeof(buffer));
+	if (!current_dir)
+	{
+		_puts("TODO: Handle getcwd failure\n");
+	}
+
+	dir = _getenv(info, "OLDPWD=");
+	if (!dir)
+	{
+		_puts(current_dir);
+		_putchar('\n');
+		return;
+	}
+
+	_puts(dir);
+	_putchar('\n');
+	chdir_ret = chdir(dir);
+	_handle_cd_result(info, chdir_ret);
+}
+
+/**
+ * _mycd - Changes the current directory of the process.
+ * @info: Structure containing potential arguments.
+ *
+ * Return: Always 0.
+ */
+int _mycd(info_t *info)
+{
+	if (!info->argv[1])
+	{
+		_handle_cd_no_argument(info);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		_handle_cd_dash_argument(info);
+	}
+	else
+	{
+		_handle_cd_argument(info);
+	}
+
+	return (0);
+}
+
+/**
+ * _myhelp - Displays help information.
+ * @info: Structure containing potential arguments.
+ *
+ * Return: Always 0.
+ */
+int _myhelp(info_t *info)
+{
+	/* TODO: Implement help functionality */
+	_puts("Help: Function not yet implemented\n");
+	return (0);
+}
+
