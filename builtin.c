3f81f2624e059f9e51cfb9557ef48d84f69235e9
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-11-30 06:49:06 -0800

    0x16. C - Simple Shell

--- builtin.c
+++ builtin.c
@@ -1,5 +1,11 @@
 #include "shell.h"
 
+// Declaration for _handle_cd_result
+static void _handle_cd_result(info_t *info, int chdir_ret);
+
+// Declaration for _handle_cd_argument
+static void _handle_cd_argument(info_t *info);
+
 /**
  * _myexit - Exits the shell.
  * @info: Structure containing potential arguments.
@@ -8,27 +14,27 @@
  */
 int _myexit(info_t *info)
 {
-	int exit_status = 0;
+    int exit_status = 0;
 
-	if (info->argv[1])
-	{
-		exit_status = _erratoi(info->argv[1]);
-		if (exit_status == -1)
-		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
-			return (1); /* Consider using a symbolic constant for error codes */
-		}
-		info->err_num = exit_status;
-	}
-	else
-	{
-		info->err_num = -1;
-	}
+    if (info->argv[1])
+    {
+        exit_status = _erratoi(info->argv[1]);
+        if (exit_status == -1)
+        {
+            info->status = 2;
+            print_error(info, "Illegal number: ");
+            _eputs(info->argv[1]);
+            _eputchar('\n');
+            return (1); /* Consider using a symbolic constant for error codes */
+        }
+        info->err_num = exit_status;
+    }
+    else
+    {
+        info->err_num = -1;
+    }
 
-	return (-2);
+    return (-2);
 }
 
 /**
@@ -37,23 +43,23 @@ int _myexit(info_t *info)
  */
 static void _handle_cd_no_argument(info_t *info)
 {
-	char *dir, buffer[1024];
-	int chdir_ret;
+    char *dir, buffer[1024];
+    int chdir_ret;
 
-	char *current_dir = getcwd(buffer, sizeof(buffer));
-	if (!current_dir)
-	{
-		_puts("TODO: Handle getcwd failure\n");
-	}
+    char *current_dir = getcwd(buffer, sizeof(buffer));
+    if (!current_dir)
+    {
+        _puts("TODO: Handle getcwd failure\n");
+    }
 
-	dir = _getenv(info, "HOME=");
-	if (!dir)
-	{
-		dir = _getenv(info, "PWD=");
-	}
+    dir = _getenv(info, "HOME=");
+    if (!dir)
+    {
+        dir = _getenv(info, "PWD=");
+    }
 
-	chdir_ret = chdir(dir ? dir : "/");
-	_handle_cd_result(info, chdir_ret);
+    chdir_ret = chdir(dir ? dir : "/");
+    _handle_cd_result(info, chdir_ret);
 }
 
 /**
@@ -62,28 +68,38 @@ static void _handle_cd_no_argument(info_t *info)
  */
 static void _handle_cd_dash_argument(info_t *info)
 {
-	char *dir;
-	char buffer[1024];
-	int chdir_ret;
+    char *dir;
+    char buffer[1024];
+    int chdir_ret;
 
-	char *current_dir = getcwd(buffer, sizeof(buffer));
-	if (!current_dir)
-	{
-		_puts("TODO: Handle getcwd failure\n");
-	}
+    char *current_dir = getcwd(buffer, sizeof(buffer));
+    if (!current_dir)
+    {
+        _puts("TODO: Handle getcwd failure\n");
+    }
 
-	dir = _getenv(info, "OLDPWD=");
-	if (!dir)
-	{
-		_puts(current_dir);
-		_putchar('\n');
-		return;
-	}
+    dir = _getenv(info, "OLDPWD=");
+    if (!dir)
+    {
+        _puts(current_dir);
+        _putchar('\n');
+        return;
+    }
 
-	_puts(dir);
-	_putchar('\n');
-	chdir_ret = chdir(dir);
-	_handle_cd_result(info, chdir_ret);
+    _puts(dir);
+    _putchar('\n');
+    chdir_ret = chdir(dir);
+    _handle_cd_result(info, chdir_ret);
+}
+
+/**
+ * _handle_cd_argument - Handles the case when an argument is provided to cd.
+ * @info: Structure containing potential arguments.
+ */
+static void _handle_cd_argument(info_t *info)
+{
+    // Implementation for handling cd with an argument
+    // Add your code here
 }
 
 /**
@@ -94,20 +110,20 @@ static void _handle_cd_dash_argument(info_t *info)
  */
 int _mycd(info_t *info)
 {
-	if (!info->argv[1])
-	{
-		_handle_cd_no_argument(info);
-	}
-	else if (_strcmp(info->argv[1], "-") == 0)
-	{
-		_handle_cd_dash_argument(info);
-	}
-	else
-	{
-		_handle_cd_argument(info);
-	}
+    if (!info->argv[1])
+    {
+        _handle_cd_no_argument(info);
+    }
+    else if (_strcmp(info->argv[1], "-") == 0)
+    {
+        _handle_cd_dash_argument(info);
+    }
+    else
+    {
+        _handle_cd_argument(info);
+    }
 
-	return (0);
+    return 0;
 }
 
 /**
@@ -116,10 +132,10 @@ int _mycd(info_t *info)
  *
  * Return: Always 0.
  */
-int _myhelp(info_t *info)
+int _myhelp(info_t *info __attribute__((unused)))
 {
-	/* TODO: Implement help functionality */
-	_puts("Help: Function not yet implemented\n");
-	return (0);
+    /* TODO: Implement help functionality */
+    _puts("Help: Function not yet implemented\n");
+    return 0;
 }
 
