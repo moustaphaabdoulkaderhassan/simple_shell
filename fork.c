ae0600f23b4eb657d9707410230503cc16bee704
moustaphaabdoulkaderhassan <moustaphaabdoulkader341@outlook.com>
2023-12-18 09:10:14 -0800

    simple shell

--- fork.c
+++ fork.c
@@ -1,58 +1,25 @@
-#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
 
-# Function to handle cleanup
-cleanup() {
-    # Your cleanup logic goes here
-    echo "Cleaning up..."
+/**
+ * main - fork example
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    pid_t my_pid;
+    pid_t pid;
+
+    printf("Before fork\n");
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("Error:");
+        return (1);
+    }
+    printf("After fork\n");
+    my_pid = getpid();
+    printf("My pid is %u\n", my_pid);
+    return (0);
 }
-
-# Trap signals to ensure cleanup on exit
-trap cleanup EXIT INT TERM
-
-# Function to execute the command
-execute_command() {
-    # Your command execution logic goes here
-    echo "Executing command: $@"
-    "$@"
-}
-
-# Main function
-main() {
-    # Fork a child process
-    (
-        # Execute the command in the child process
-        execute_command "$@"
-
-        # Capture the exit code of the command
-        exit_code=$?
-
-        # Write the exit code to a status file
-        echo "$exit_code" > "$STATUS_FILE"
-
-        # Exit the child process
-        exit $exit_code
-    ) &
-
-    # Wait for the child process to finish
-    wait $!
-
-    # Read the exit code from the status file
-    exit_code=$(<"$STATUS_FILE")
-
-    # Print the exit code
-    echo "Exit code: $exit_code"
-
-    # Your additional logic based on the exit code goes here
-    # For example, you can check the exit code against expected values
-    # and perform further actions.
-
-    # Return the exit code from the main function
-    return $exit_code
-}
-
-# Set up variables
-STATUS_FILE="/tmp/child_exit_status"
-
-# Example usage
-main echo "Hello, World!"
-
